880a38c3fec90d53f7d31aa63b66080f6658093e
FurnitureSet <65529437+FurnitureSet@users.noreply.github.com>
2025-10-18 13:07:51 +0900

    GameManager Singleton, onBodyChosen Event

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,13 +9,19 @@ public class GameManager : MonoBehaviour
         Paused = 0,
         Playing = 1,
         ChoosingBody = 2,
+        CheckingAnswer = 3,
     }
 
+    public static GameManager Instance {  get; private set; }
+    
     public GameState state;
     
     public Body[] bodies;
     public int currentBodyIndex = -1;
+    public Body CurrentBody => bodies[currentBodyIndex];
 
+    public UnityEvent<Body> onBodyChosen;
+    
     private HashSet<int> _visitedBodies;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -36,16 +43,30 @@ public class GameManager : MonoBehaviour
             case GameState.ChoosingBody:
                 ChooseBody();
                 break;
+            case GameState.CheckingAnswer:
+                break;
             case GameState.Playing:
                 break;
+            case GameState.Paused:
             default:
                 return;
         }
         
-        
 
     }
 
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    
     void ChooseBody()
     {
         int newBodyIndex = -1;
@@ -55,6 +76,7 @@ public class GameManager : MonoBehaviour
         } while (currentBodyIndex == newBodyIndex);
         
         currentBodyIndex = newBodyIndex;
+        onBodyChosen.Invoke(bodies[currentBodyIndex]);
         state = GameState.Playing;
     }
 }
