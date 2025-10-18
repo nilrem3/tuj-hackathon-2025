896d09a55d5131bbabf31c1c47bd32fa209b67e7
Mikuepe <hunterbench@gmail.com>
2025-10-18 16:56:15 +0900

    Add Button to Selection UI

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -31,6 +31,8 @@ public class GameManager : MonoBehaviour
     public CurveRenderer lightCurveRenderer;
     public CurveRenderer spectralRenderer;
     
+    private 
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
