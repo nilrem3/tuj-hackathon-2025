1cdaeada6fa73b87fed241a387cc831feb4d161c
FurnitureSet <65529437+FurnitureSet@users.noreply.github.com>
2025-10-18 13:30:08 +0900

    Body Types have Shape/Material

--- Assets/Bodies/Body.cs
+++ Assets/Bodies/Body.cs
@@ -3,13 +3,34 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Body", menuName = "Scriptable Objects/Body")]
 public class Body : ScriptableObject
 {
+    public enum Shape
+    {
+        Sphere = 0,
+        Cylinder = 1,
+        Box = 2,
+        Lumpy = 3,
+    }
+
+    public enum Material
+    {
+        Ice = 0,
+        Iron = 1,
+        Gravel = 2,
+        Hydrogen = 3, 
+        VolcanicAsh = 4,
+        Wood = 5,
+    }
+    
     public int sizeActual;
     public int distanceActual;
     
     public MeshFilter meshFilter;
+    public MeshRenderer meshRenderer;
+    
+    public Shape shape;
     public BrightnessCurve brightnessCurve;
     
-    public MeshRenderer meshRenderer;
+    public Material material;
     public LightQuality lightQuality;
 
     public int luminosityTotal;
